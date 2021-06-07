5f81cde9dc3d2430c34674833d0ca06c3177346f
Javier Arellano <joma_lp_18@hotmail.com>
2021-06-06 22:59:33 -0500

    Add files via upload

---
+++ survey_PDO/vistas/vistavotacion.php
@@ -0,0 +1,6 @@
+<input type="radio" name="lenguaje" id="" value="c">C <br>   
+<input type="radio" name="lenguaje" id="" value="c++">C++ <br>
+<input type="radio" name="lenguaje" id="" value="java">Java <br>         
+<input type="radio" name="lenguaje" id="" value="swift">Swift <br>         
+<input type="radio" name="lenguaje" id="" value="python">Python <br>         
+<input type="submit" value="Votar!">
